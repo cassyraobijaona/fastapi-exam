a94c2afbb535cf6fb2f53d9a705dc9d558873898
cassy <cassyraobijaona@gmail.com>
2025-09-03 12:12:06 +0300

    Reponses de l'examen

--- main.py
+++ main.py
@@ -17,6 +17,43 @@ def custom_openapi():
 app.openapi = custom_openapi
 
 
+class Characteristics(Attribus):
+    max_speed: float
+    max_fuel_capacity: float
+
+class Car(Attribus):
+    id: str
+    brand: str
+    model: str
+    characteristics: Characteristics
+
+cars_db: Dict[str, Car] = {}
+
 @app.get("/ping")
 def ping():
-    return {"message": "pong"}
+    return "pong"
+
+@app.post("/cars", status_code=201)
+def create_car(car: Car):
+    cars_db[car.id] = car
+    return car
+
+@app.get("/cars", response_model=List[Car])
+def get_all_cars():
+    return list(cars_db.values())
+
+@app.get("/cars/{id}", response_model=Car)
+def get_car_by_id(id: str):
+    car = cars_db.get(id)
+    if not car:
+        raise HTTPException(status_code=404, detail=f"Car with ID '{id}' not found.")
+    return car
+
+@app.put("/cars/{id}/characteristics", response_model=Car)
+def update_characteristics(id: str, new_char: Characteristics):
+    car = cars_db.get(id)
+    if not car:
+        raise HTTPException(status_code=404, detail=f"Car with ID '{id}' not found.")
+    car.characteristics = new_char
+    cars_db[id] = car
+    return car
