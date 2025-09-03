89079e6071cf6bdb0bbe5c2b995e19791dcb2ab2
cassy <cassyraobijaona@gmail.com>
2025-09-03 08:41:35 +0300

    Ajout du projet FastAPI (ping route)

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+import yaml
 
 app = FastAPI()
+
+with open("main.yaml", "r") as f:
+    main_yaml = yaml.safe_load(f)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    app.openapi_schema = main_yaml
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
 
 @app.get("/ping")
 def ping():
